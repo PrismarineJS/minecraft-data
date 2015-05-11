a927beb211d1ab29e969ba5539eb88f13c6a8f6f
Romain Beaumont <romain.rom1@gmail.com>
2015-05-12 00:19:41 +0200

    remove files (not needed in gh-pages branch)

--- doc/history.md
+++
@@ -1,6 +0,0 @@
-## 0.0.1
-
- * first version
- * enums in enums/
- * scripts to audit and generate the enums in bin/
- * support minecraft 1.8 with some missing data
--- index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  blocks: require('./enums/blocks'),
-  biomes: require('./enums/biomes'),
-  items: require('./enums/items'),
-  recipes: require('./enums/recipes'),
-  instruments: require('./enums/instruments'),
-  materials: require('./enums/materials')
-};
