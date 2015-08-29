bb9cdba35e9cd4198ee959bc5acca8bef70e4a3c
roblabla <robinlambertz+dev@gmail.com>
2015-08-29 18:57:27 +0000

    Test of new table-based layout

--- showValues.js
+++ showValues.js
@@ -1,3 +1,5 @@
+var _ = DOMBuilder;
+
 $j(document).ready(function() {
 
   loadItems();
@@ -71,35 +73,109 @@ function loadInstruments()
   );
 }
 
+function flatten(array, mutable) {
+    var result = [];
+    var nodes = (mutable && array) || array.slice();
+    var node;
+
+    if (!array.length) {
+        return result;
+    }
+
+    node = nodes.pop();
+
+    do {
+        if (Array.isArray(node))
+            nodes.push.apply(nodes, node);
+        else
+            result.push(node);
+    } while (nodes.length && (node = nodes.pop()) !== undefined);
+
+    result.reverse(); // we reverse result to restore the original order
+    return result;
+}
+
 function protocolToString(protocol)
 {
-  return '<div id="protocolActualTable">' +
-  Object.keys(protocol)
-    .map(function(directionsName){return "<h1>"+directionsName+"</h1>\n"+directionsToString(protocol[directionsName]);})
-    .join("\n")+
-  '</div>';
+  return _('div#protocolActualTable')._(
+    flatten(Object.keys(protocol).map(function (state) {
+      return [_('h1').text(state),
+        directionsToString(state, protocol[state])];
+    }))
+  ).H();
 }
 
-function directionsToString(directions)
+function directionsToString(state, directions)
 {
-  return "<h2>toClient</h2>\n"+directionToString(directions["toClient"])+"\n"
-    + "<h2>toServer</h2>\n"+directionToString(directions["toServer"])+"\n";
+  return [_('h2').text('toClient'), directionToString(state, "toClient", directions["toClient"]),
+          _('h2').text('toServer'), directionToString(state, "toServer", directions["toServer"])];
 }
 
-function directionToString(direction)
+function directionToString(state, direction, packets)
 {
-  return Object.keys(direction).map(function(packetName){
-    return "<h3>"+packetName+" : "+direction[packetName].id+"</h3>\n"+packetToString(direction[packetName])
-  }).join("\n");
+  return Object.keys(packets).map(function(packetName){
+    return [_('h3').text(packetName + ' : ' + packets[packetName].id),
+            packetToString(state, direction, packets[packetName])];
+  })
 }
 
-function packetToString(packet)
+function countRows(packet) {
+  return packet.fields.length;
+}
+
+function getFieldInfo(type) {
+  if (typeof type === "string")
+    return { "type": type };
+  else if (Array.isArray(type))
+    return { "type": type[0], "typeArgs": type[1] };
+  else
+    return type;
+}
+
+function packetToString(state, direction, packet)
 {
-  return "<ul>"+
-      packet["fields"]
-        .map(function(field){return "<li>"+field["name"]+" : "+field["type"]+"</li>"})
-        .join("\n")
-    +"</ul>"
+  var rows = countRows(packet);
+  if (rows == 0)
+    rows = 1;
+  function fieldLine(field) {
+    var fieldType = getFieldInfo(field.type);
+    if (fieldType.type === 'array') {
+      return [_('td').T(field.name), _('td').T("array<" + fieldType.typeArgs.countType + "," + fieldType.typeArgs.type + ">")];
+    } else {
+      return [_('td').T(field.name), _('td').T(field.type)];
+    }
+  }
+  function generateFirstLine() {
+    var rslt = [
+      _('td', { rowspan: rows }).T(packet.id),
+      _('td', { rowspan: rows }).T(state),
+      _('td', { rowspan: rows }).T(direction)
+    ];
+    if (packet.fields.length > 0)
+      rslt = rslt.concat(fieldLine(packet.fields[0]));
+    else
+      rslt = rslt.concat([_('td', { colspan: 2 })._([_('i').T('no fields')])]);
+    return rslt;
+  }
+  function generateLines() {
+    return packet.fields.slice(1).reduce(function (acc, field) {
+      acc.push(_('tr')._(fieldLine(field)));
+      return acc;
+    }, [_('tr')._(generateFirstLine())]);
+  }
+  return _('table.packet')
+    ._([
+      _('thead')._([
+        _('tr')._([
+          _('th').T('Packet ID'),
+          _('th').T('State'),
+          _('th').T('Bound To'),
+          _('th').T('Field Name'),
+          _('th').T('Field Type')
+        ])
+      ]),
+      _('tbody')._(generateLines())
+    ]);
 }
 
 function loadProtocol()
