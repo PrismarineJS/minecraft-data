05307c39898c9cebbfe8af1a18069bd9016d3a45
Romain Beaumont <romain.rom1@gmail.com>
2015-08-30 06:53:09 +0200

    fix scrolling in firefox and put scrolling it in a separate file

---
+++ scroll.js
@@ -0,0 +1,10 @@
+function scroll()
+{
+  if(window.location.hash=="") return;
+  var anchor=$j('a[href="'+window.location.hash+'"]');
+  if(anchor) $j(window).scrollTop(anchor.offset().top);
+}
+function done(){
+  scroll();
+  $j("a").click(function(){setTimeout(scroll,0);setTimeout(scroll,50)});
+}
