620537ce6426b62413017493cd661042571a9421
roblabla <robinlambertz+dev@gmail.com>
2016-06-06 15:39:09 +0200

    Unfinished work

--- display_schema.js
+++ display_schema.js
@@ -2,6 +2,7 @@ var docson=require("docson");
 
 function displaySchema(enums) {
   return Promise.all(enums.map(function(json){
+    if (json === "protocol") return;
     return docson.doc(json, require("minecraft-data").schemas[json]);
   }));
 }
