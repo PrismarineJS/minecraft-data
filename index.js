223cf0c2ef5fc12d3315e6f03d60c05ba55c5a83
Romain Beaumont <romain.rom1@gmail.com>
2016-11-06 02:02:10 +0100

    generate version list using mcdata

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var displaySchema=require("./display_schema");
 var showValues=require("./showValues");
 var scrollToAnchor=require("./scroll");
 var tabs=require("./tabs");
+var versionsLinks=require("./versionLinks");
 
+versionsLinks();
 tabs(data.active,data.enums,data.enumsValues);
 displaySchema(data.enums).then(scrollToAnchor);
 showValues(data.version);
