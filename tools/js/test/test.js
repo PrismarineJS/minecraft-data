18f8531e5c1013156ae8fe3b9d5385369f9932f3
TheDudeFromCI <thedudefromci@gmail.com>
2020-08-16 14:28:23 +0000

    Add loot tables to test
    Signed-off-by: TheDudeFromCI <thedudefromci@gmail.com>

--- tools/js/test/test.js
+++ tools/js/test/test.js
@@ -11,7 +11,7 @@ const Validator = require('protodef-validator')
 
 Error.stackTraceLimit = 0
 
-const data = ['biomes', 'instruments', 'items', 'materials', 'blocks', 'blockCollisionShapes', 'recipes', 'windows', 'entities', 'protocol', 'version', 'effects', 'enchantments', 'language', 'foods', 'particles']
+const data = ['biomes', 'instruments', 'items', 'materials', 'blocks', 'blockCollisionShapes', 'recipes', 'windows', 'entities', 'protocol', 'version', 'effects', 'enchantments', 'language', 'foods', 'particles', 'blockLoot', 'entityLoot']
 
 require('./version_iterator')(function (p, versionString) {
   describe('minecraft-data schemas ' + versionString, function () {
