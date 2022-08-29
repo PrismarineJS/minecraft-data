dbfb18bf14adea1e3649cf0bc4d1365d7b12de43
extremeheat <extreme@protonmail.ch>
2022-08-29 05:25:33 -0400

    Update web branch (#625)
    * minify web bundle
    
    * Allow version switching without reload, fix bedrock protocol anchor

--- data.js
+++ data.js
@@ -1,19 +1,19 @@
-const queryString = require('query-string')
+module.exports = () => {
+  const parameters = Object.fromEntries(new URLSearchParams(window.location.search))
 
-const parameters = queryString.parse(window.location.search)
+  const rawVersion = parameters['v']
+  const version = rawVersion || '1.16.1'
+  const rawActive = parameters['d']
+  const active = rawActive || 'items'
 
-const rawVersion = parameters['v']
-const version = rawVersion || '1.16.1'
-const rawActive = parameters['d']
-const active = rawActive || 'items'
+  const enums = ['biomes', 'instruments', 'items', 'materials', 'blocks', 'recipes', 'entities', 'protocol', 'windows', 'effects']
 
-const enums = ['biomes', 'instruments', 'items', 'materials', 'blocks', 'recipes', 'entities', 'protocol', 'windows', 'effects']
+  const enumsValues = ['biomes', 'instruments', 'items', 'blocks', 'entities', 'protocol', 'windows', 'effects']
 
-const enumsValues = ['biomes', 'instruments', 'items', 'blocks', 'entities', 'protocol', 'windows', 'effects']
-
-module.exports = {
-  version: version,
-  active: active,
-  enums: enums,
-  enumsValues: enumsValues
+  return {
+    version: version,
+    active: active,
+    enums: enums,
+    enumsValues: enumsValues
+  }
 }
