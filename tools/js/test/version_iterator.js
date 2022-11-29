2db2fc4a07e42c3b1aa662f1ce2a6157747ba86c
U9G <git@u9g.dev>
2022-11-29 09:48:48 -0500

    Test: Find versions to test iteratively

--- tools/js/test/version_iterator.js
+++ tools/js/test/version_iterator.js
@@ -6,9 +6,28 @@ const minecraftTypes = ['pc', 'bedrock']
 
 function forEach (f) {
   minecraftTypes.forEach(function (type) {
-    const versions = require('../../../data/' + type + '/common/versions')
-    versions.forEach(function (version) {
+    getVersionsToTest(type).forEach(function (version) {
       f(path.join(__dirname, '../../../data', type, version), type + ' ' + version)
     })
   })
+}
+
+function getVersionsToTest (minecraftType) {
+  const dataPathsForMinecraftType = require('../../../data/dataPaths.json')[minecraftType]
+
+  const versions = new Set()
+  for (const pathsForVersion of Object.values(dataPathsForMinecraftType)) {
+    for (const versionPath of Object.values(pathsForVersion)) {
+      const [mcType, mcVer] = versionPath.split('/')
+      if (mcType === minecraftType) {
+        // TODO: We need to just replace this with the latest version
+        //       as a string with numbers although more than likely
+        //       this is already going to be already, so probably unnecessary
+        if (mcVer === 'latest') continue
+        versions.add(mcVer)
+      }
+    }
+  }
+
+  return versions
 }
