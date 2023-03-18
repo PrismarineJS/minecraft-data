150e8968e71f416cbe8d25f010318e700e0de252
extremeheat <extreme@protonmail.ch>
2023-03-18 15:21:41 -0400

    add version helper bot

---
+++ .github/helper-bot/index.js
@@ -0,0 +1,157 @@
+const fs = require('fs')
+const cp = require('child_process')
+const https = require('https')
+const helper = require('./github-helper')
+const pcManifestURL = 'https://launchermeta.mojang.com/mc/game/version_manifest.json'
+const changelogURL = 'https://feedback.minecraft.net/hc/en-us/sections/360001186971-Release-Changelogs'
+
+function fetch(url, filePathIfOnDisk) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      let data = filePathIfOnDisk ? fs.createWriteStream(filePathIfOnDisk) : ''
+      const expectedContentLength = res.headers['content-length']
+      let readSoFar = 0
+      res.on('data', (chunk) => {
+        if (filePathIfOnDisk) {
+          data.write(chunk)
+          readSoFar += chunk.length
+          if (readSoFar === chunk.length) console.log(`Downloaded ${readSoFar} of ${expectedContentLength} bytes for ${filePathIfOnDisk}`)
+        }
+        else data += chunk
+      })
+      res.on('end', () => {
+        if (filePathIfOnDisk) resolve(data.end())
+        else resolve({
+          ok: true,
+          text: () => Promise.resolve(data),
+          json: () => Promise.resolve(JSON.parse(data))
+        })
+      })
+    }).on('error', reject)
+  })
+}
+
+function buildFirstIssue (title, result, jarData) {
+  let protocolVersion = jarData?.protocol_version || 'Failed to obtain from JAR'
+  const name = jarData?.name || result.id
+  const date = result.releaseTime
+
+  return {
+    title,
+    body: `
+A new Minecraft Java Edition version is available (as of ${date}), version **${result.id}**
+## Official Changelog
+* ${changelogURL}
+## Protocol Details
+(I will close this issue automatically if "${result.id}" is added to data/pc/common/versions.json on "master")
+<table>
+  <tr><td><b>Name</b></td><td>${name}</td>
+  <tr><td><b>Protocol ID</b></td><td>${protocolVersion}</td>
+  <tr><td><b>Release Date</b></td><td>${date}</td>
+  <tr><td><b>Release Type</b></td><td>${result.type}</td>
+  <tr><td><b>Data Version</b></td><td>${jarData?.world_version}</td>
+  <tr><td><b>Java Version</b></td><td>${jarData?.java_version}</td>
+</table>
+<hr/>
+🤖 I am a bot, I check for updates every 2 hours without a trigger. You can close this issue to prevent any further updates.
+    `
+  }
+}
+
+const protocolVersions = {
+  pc: require('../../data/pc/common/protocolVersions.json'),
+  bedrock: require('../../data/bedrock/common/protocolVersions.json')
+}
+const supportedVersions = {
+  pc: require('../../data/pc/common/versions.json'),
+  bedrock: require('../../data/bedrock/common/versions.json')
+}
+
+async function updateManifestPC() {
+  const manifest = await fetch(pcManifestURL).then(res => res.json())
+  // fs.writeFileSync('./manifest.json', JSON.stringify(manifest, null, 2))
+  const knownVersions = protocolVersions.pc.reduce((acc, cur) => (acc[cur.minecraftVersion] = cur, acc), {})
+  const latestRelease = manifest.latest.release
+
+  const title = `Support Minecraft PC ${latestRelease}`
+  const issueStatus = await helper.getIssueStatus(title)
+
+  if (supportedVersions.pc.includes(latestRelease)) {
+    if (issueStatus.open) {
+      helper.close(issueStatus.id, `Closing as PC ${latestRelease} is now supported`)
+    }
+    console.log('Latest PC version is supported.')
+    return
+  } else if (knownVersions[latestRelease]) {
+    console.log(`Latest PC version ${latestRelease} is known in protocolVersions.json, but not in versions.json (protocol version ${knownVersions[latestRelease].version})`)
+    return
+  } else {
+    console.log(`Latest PC version ${latestRelease} is not known in protocolVersions.json, adding and making issue`)
+  }
+  const latestReleaseData = manifest.versions.find(v => v.id === latestRelease)
+
+  // Note: We don't use the below check to track if the version is supported properly or not
+  // (data like protocol/blocks/items/etc is present), just to make sure the known protocol version is correct.
+  try {
+    const latestReleaseManifest = await fetch(latestReleaseData.url).then(res => res.json())
+    // fs.writeFileSync(`./${latestRelease}.json`, JSON.stringify(latestReleaseManifest, null, 2))
+    // Download client jar
+    if (!fs.existsSync(`./${latestRelease}.jar`)) {
+      const clientJarUrl = latestReleaseManifest.downloads.client.url
+      console.log('Downloading client jar', clientJarUrl)
+      await fetch(clientJarUrl, `./${latestRelease}.jar`)
+    }
+    console.log(`Unzipping client jar ./${latestRelease}.jar's version.json data`)
+    // unzip with tar (works on windows too)
+    cp.execSync(`tar -xf ${latestRelease}.jar version.json`)
+    const versionJson = require('./version.json')
+
+    let majorVersion
+    try {
+      majorVersion = versionJson.id.split('.', 2).join('.')
+    } catch (e) {
+      console.error('Failed to get major version from version.json.id', versionJson.id, ', falling back to protocolVersions.json latest (may be incorrect)')
+      majorVersion = protocolVersions.pc[0].majorVersion
+    }
+
+    const newEntry = {
+      minecraftVersion: versionJson.id,
+      version: versionJson.protocol_version,
+      dataVersion: versionJson.world_version,
+      usesNetty: true,
+      majorVersion,
+      releaseType: latestReleaseData.type
+    }
+    console.log('Adding new entry to pc protocolVersions.json', newEntry)
+    const updatedProtocolVersions = [newEntry, ...protocolVersions.pc]
+    fs.writeFileSync('../../data/pc/common/protocolVersions.json', JSON.stringify(updatedProtocolVersions, null, 2))
+
+    if (process.env.CI) {
+      console.log('Committing changes to protocolVersions.json')
+      // https://github.com/actions/checkout/pull/1184
+      cp.execSync('git config user.name "github-actions[bot]"')
+      cp.execSync('git config user.email "41898282+github-actions[bot]@users.noreply.github.com"')
+      cp.execSync('git add ../../data/pc/common/protocolVersions.json')
+      cp.execSync(`git commit -m "Add ${versionJson.id} to pc protocolVersions.json"`)
+    }
+
+    console.log('Opening issue', versionJson)
+    const issuePayload = buildFirstIssue(title, latestReleaseData, versionJson)
+
+    helper.createIssue(issuePayload)
+
+    fs.writeFileSync('./issue.md', issuePayload.body)
+    console.log('OK, wrote to ./issue.md', issuePayload)
+  } catch (e) {
+    console.error(e)
+
+    console.log('Latest PC version is not supported and we failed to load data. Opening issue...')
+    const issuePayload = buildFirstIssue(title, latestReleaseData)
+    helper.createIssue(issuePayload)
+
+    fs.writeFileSync('./issue.md', issuePayload.body)
+    console.log('OK, wrote to ./issue.md', issuePayload)
+  }
+}
+
+updateManifestPC()
