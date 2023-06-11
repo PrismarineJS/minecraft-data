a2208fb48b87f68f767c8821b685701aed04ca8f
extremeheat <extreme@protonmail.ch>
2023-06-11 07:13:03 -0400

    Fix pages build failing due to bundle size (#716)
    * exclude unused large data from bundle
    
    * use browserify via API as stubs are needed
    
    * fix pc1.19.4 biomes, issue with bedrock protocol paths
    
    * fix lint

---
+++ buildBundle.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const browserify = require('browserify')
+const through = require('through')
+
+const blockedFiles = ['steve.json', 'blockMappings.json', 'blockCollisionShapes.json', 'blocksJ2B.json', 'blocksB2J.json']
+const stubs = { 'blockCollisionShapes.json': '{"blocks":{},"shapes":{}}' }
+
+const bundle = fs.createWriteStream('bundle.js')
+const bundler = browserify()
+bundler.transform(function (file, options) {
+  for (const blockedFile of blockedFiles) {
+    if (file.endsWith(blockedFile)) {
+      console.log('Blocked', file)
+      return through(function write () {
+        // no op
+      }, function end () {
+        this.queue(stubs[blockedFile] || '[]')
+        this.queue(null)
+      })
+    }
+  }
+  return through()
+}, { global: true })
+bundler.add('./index.js')
+bundler.bundle().pipe(bundle)
