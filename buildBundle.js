c8804dc847aa6904ff13fa63f40468db4efdc048
extremeheat <extreme@protonmail.ch>
2023-11-25 01:07:53 +0000

    Remove json-stable-stringify dep

--- buildBundle.js
+++ buildBundle.js
@@ -2,11 +2,16 @@ const fs = require('fs')
 const browserify = require('browserify')
 const through = require('through')
 
-const blockedFiles = ['steve.json', 'blockMappings.json', 'blockCollisionShapes.json', 'blocksJ2B.json', 'blocksB2J.json']
+const blockedFiles = ['steve.json', 'blockMappings.json', 'blockCollisionShapes.json', 'blocksJ2B.json', 'blocksB2J.json', 'blocks_models.json', 'blocks_states.json', 'entityLoot.json', 'blockLoot.json', 'language.json', 'blockStates.json', 'commands.json']
 const stubs = { 'blockCollisionShapes.json': '{"blocks":{},"shapes":{}}' }
+
+function len(file) {
+  return fs.statSync(file).size
+}
 
 const bundle = fs.createWriteStream('bundle.js')
 const bundler = browserify()
+const files = []
 bundler.transform(function (file, options) {
   for (const blockedFile of blockedFiles) {
     if (file.endsWith(blockedFile)) {
@@ -19,7 +24,12 @@ bundler.transform(function (file, options) {
       })
     }
   }
+  console.log('Using', file, len(file), 'bytes')
+  files.push([len(file), file])
   return through()
 }, { global: true })
 bundler.add('./index.js')
 bundler.bundle().pipe(bundle)
+.addListener('finish', () => {
+  console.log('Biggest: ', files.sort((a, b) => b[0] - a[0]))
+})
